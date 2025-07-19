0180fb15511c805ec505785f11313699b6a280b9
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2025-07-19 23:50:25 +0200

    Update zip-native-dependencies.js
    Upload everything

--- scripts/zip-native-dependencies.js
+++ scripts/zip-native-dependencies.js
@@ -20,28 +20,56 @@ const fs = require('fs');
 const path = require('path');
 const archiver = require('archiver');
 
+// async function run() {
+//     const repoPath = path.resolve(__dirname, '..');
+//     const zipFile = path.join(__dirname, `native-dependencies-${process.platform}-${process.arch}.zip`);
+//     const browserAppPath = path.join(repoPath, 'examples', 'browser');
+//     const nativeDependencies = await glob('lib/backend/native/**', {
+//         cwd: browserAppPath
+//     });
+//     const buildDependencies = await glob('lib/build/Release/**', {
+//         cwd: browserAppPath
+//     });
+//     const trashDependencies = await glob('lib/backend/{windows-trash.exe,macos-trash}', {
+//         cwd: browserAppPath
+//     });
+//     const archive = archiver('zip');
+//     const output = fs.createWriteStream(zipFile, { flags: "w" });
+//     archive.pipe(output);
+//     for (const file of [
+//         ...nativeDependencies,
+//         ...buildDependencies,
+//         ...trashDependencies
+//     ]) {
+//         const filePath = path.join(browserAppPath, file);
+//         archive.file(filePath, {
+//             name: file,
+//             mode: (await fs.promises.stat(filePath)).mode
+//         });
+//     }
+//     await archive.finalize();
+// }
+
 async function run() {
     const repoPath = path.resolve(__dirname, '..');
     const zipFile = path.join(__dirname, `native-dependencies-${process.platform}-${process.arch}.zip`);
-    const browserAppPath = path.join(repoPath, 'examples', 'browser');
-    const nativeDependencies = await glob('lib/backend/native/**', {
-        cwd: browserAppPath
+    //const browserAppPath = path.join(repoPath, 'examples', 'browser');
+    const nativeDependencies = await glob('**/*', {
+        cwd: repoPath
     });
-    const buildDependencies = await glob('lib/build/Release/**', {
-        cwd: browserAppPath
-    });
-    const trashDependencies = await glob('lib/backend/{windows-trash.exe,macos-trash}', {
-        cwd: browserAppPath
-    });
+    // const buildDependencies = await glob('lib/build/Release/**', {
+    //     cwd: browserAppPath
+    // });
+    // const trashDependencies = await glob('lib/backend/{windows-trash.exe,macos-trash}', {
+    //     cwd: browserAppPath
+    // });
     const archive = archiver('zip');
     const output = fs.createWriteStream(zipFile, { flags: "w" });
     archive.pipe(output);
     for (const file of [
-        ...nativeDependencies,
-        ...buildDependencies,
-        ...trashDependencies
+        ...nativeDependencies
     ]) {
-        const filePath = path.join(browserAppPath, file);
+        const filePath = path.join(repoPath, file);
         archive.file(filePath, {
             name: file,
             mode: (await fs.promises.stat(filePath)).mode
